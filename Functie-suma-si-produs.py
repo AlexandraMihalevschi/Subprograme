3c3bc056d0411ff3bc211890195d3aedb01ecd81
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2022-09-14 10:13:49 +0300

    Update Functie-suma-si-produs.py

--- Functie-suma-si-produs.py
+++ Functie-suma-si-produs.py
@@ -6,15 +6,13 @@ d = int(input("Numitorul 2 fractie: "))
 def adunare(x, y, z, p):
     if z*p!=0:
         s = ((x*p)+(y*z))/(z*p)
-        return s
-    else: print("Nu exista")
+    return s
     
 
 def inmultire(x, y, z, p):
     if y*p!=0:
         p = (x*z)/(y*p)
-        return p
-    else: print("Nu exista")
+    return p
     
 
 print("Suma este ", round(adunare(a, b, c, d), 2), " iar produsul este ", round(inmultire(a, b, c, d), 2))
